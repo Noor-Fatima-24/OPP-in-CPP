cf1031c2f9ca7f232a411bc9383ff804d413ba03
noor fatima <kaspi0965@gmail.com>
2025-11-03 10:20:10 +0500

    OOP_IN_CPP

---
+++ 04_Polymorphism/function_overloading.cpp
@@ -0,0 +1,29 @@
+// 📘 Topic: Function Overloading (Compile-time Polymorphism)
+// ----------------------------------------------------------
+// Same function name with different parameters.
+
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+};
+
+int main() {
+    Calculator c;
+    cout << c.add(2, 3) << endl;
+    cout << c.add(2.5, 3.7) << endl;
+    cout << c.add(1, 2, 3) << endl;
+    return 0;
+}
